0a338e5ca3dddb481bbe34f3a68ddca980886518
romaklimenko <romaklimenko@gmail.com>
2023-05-21 15:09:12 +0200

    get_entity_blob, get_entity_as_clue #16

---
+++ cluedin/entity.py
@@ -0,0 +1,68 @@
+import requests
+
+from .context import Context
+from .env import CLUEDIN_REQUEST_TIMEOUT_IN_SECONDS
+
+
+def get_entity_blob(context: Context, entity_id: str) -> str:
+    """Get entity blob.
+
+    Args:
+        context (Context): Context object.
+        entity_id (str): Entity ID.
+
+    Returns:
+        dict: XML response - entity blob.
+    """
+
+    headers = {
+        'Authorization': f'Bearer {context.access_token}'
+    }
+
+    params = {
+        'id': entity_id
+    }
+
+    response = requests.get(
+        url=f'{context.api_url}/admin/entity/blob',
+        headers=headers,
+        params=params,
+        timeout=CLUEDIN_REQUEST_TIMEOUT_IN_SECONDS,
+        verify=context.verify_tls)
+
+    if not response.ok:
+        response.raise_for_status()
+
+    return response.text
+
+
+def get_entity_as_clue(context: Context, entity_id: str) -> str:
+    """Get entity as clue.
+
+    Args:
+        context (Context): Context object.
+        entity_id (str): Entity ID.
+
+    Returns:
+        dict: XML response - entity clue.
+    """
+
+    headers = {
+        'Authorization': f'Bearer {context.access_token}'
+    }
+
+    params = {
+        'id': entity_id
+    }
+
+    response = requests.get(
+        url=f'{context.api_url}/admin/entity/clue',
+        headers=headers,
+        params=params,
+        timeout=CLUEDIN_REQUEST_TIMEOUT_IN_SECONDS,
+        verify=context.verify_tls)
+
+    if not response.ok:
+        response.raise_for_status()
+
+    return response.text
---
+++ tests/test_entity.py
@@ -0,0 +1,37 @@
+import os
+
+# pylint: disable=wrong-import-order
+from .ctx import cluedin
+from cluedin import Context
+
+
+class TestEntity:
+    # pylint: disable=missing-docstring
+
+    def test_get_entity_blob(self):
+
+        # Arrange
+        context = Context.from_json_file(os.environ['CLUEDIN_CONTEXT'])
+        context.get_token()
+        
+        entity_id = cluedin.account.get_users(context)[0]['Entity']['entity']['attribute-id']
+
+        # Act
+        blob = cluedin.entity.get_entity_blob(context, entity_id)
+
+        # Assert
+        assert '<?xml version="1.0" encoding="utf-8"?>' in blob.partition('\n')[0]
+
+    def test_get_entity_as_clue(self):
+
+        # Arrange
+        context = Context.from_json_file(os.environ['CLUEDIN_CONTEXT'])
+        context.get_token()
+        
+        entity_id = cluedin.account.get_users(context)[0]['Entity']['entity']['attribute-id']
+
+        # Act
+        blob = cluedin.entity.get_entity_as_clue(context, entity_id)
+
+        # Assert
+        assert '<?xml version="1.0" encoding="utf-8"?>' in blob.partition('\n')[0]
