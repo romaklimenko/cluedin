9e9050f580b7dcdb24f3dad8682d919455547d03
romaklimenko <romaklimenko@gmail.com>
2024-02-24 18:54:16 +0100

    Get a Rule. Close #31

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -25,3 +25,20 @@ class TestRules:
         assert 'total' in rules['data']['management']['rules']
         assert rules['data']['management']['rules']['total'] == 2
         assert len(rules['data']['management']['rules']['data']) == 2
+
+    def test_get_rule(self):
+
+        # Arrange
+        context = Context.from_json_file(os.environ['CLUEDIN_CONTEXT'])
+        context.get_token()
+        rule_id = "e283807e-3da8-4183-9425-c91b39a611ae"  # Replace with an actual rule ID
+
+        # Act
+        rule = cluedin.rules.get_rule(context, rule_id)
+
+        # Assert
+        assert rule is not None
+        assert 'data' in rule
+        assert 'management' in rule['data']
+        assert 'rule' in rule['data']['management']
+        assert 'condition' in rule['data']['management']['rule']
