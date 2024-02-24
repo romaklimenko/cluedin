ecdce0a4f0788c82fba5436453a5bdb73a60a1cc
romaklimenko <romaklimenko@gmail.com>
2024-02-24 17:25:32 +0100

    formatting

--- tests/ctx.py
+++ tests/ctx.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# pylint: disable=wrong-import-position, unused-import
-import cluedin
+import cluedin  # pylint: disable=unused-import
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
