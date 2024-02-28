0c69b7d083503b09c5717617cd9c290273a21c32
romaklimenko <romaklimenko@gmail.com>
2024-02-28 19:48:23 +0100

    Custom Operators. Close #38

--- cluedin/rules/rule_group.py
+++ cluedin/rules/rule_group.py
@@ -1,62 +1,12 @@
-from .rule import Rule
-
-
 class RuleGroup:
     """
-    Represents a group of rules that can be evaluated based on a condition.
-
-    Args:
-        rule_group (dict): A dictionary containing the rule group data.
+    Represents a group of rules.
 
     Attributes:
-        condition (str): The condition for evaluating the rules ('AND' or 'OR').
-        rules (list): A list of rules to be evaluated.
-
-    Methods:
-        evaluate(obj): Evaluates the rule group based on the condition and returns a boolean result.
-        get_rule_object(rule): Returns the rule object based on the given rule data.
-
-    Raises:
-        ValueError: If an invalid condition is provided.
-
+        condition (str): The condition for the rule group. Can be 'AND' or 'OR'.
+        rules (list): The list of rules in the group.
     """
 
     def __init__(self, rule_group):
         self.condition = rule_group['condition']
         self.rules = rule_group['rules']
-
-    def evaluate(self, obj):
-        """
-        Evaluates the rule group based on the condition.
-
-        Args:
-            obj: The object to be evaluated against the rules.
-
-        Returns:
-            bool: True if the rule group evaluates to True, False otherwise.
-
-        Raises:
-            ValueError: If the condition is invalid.
-        """
-        if self.condition == 'AND':
-            return all(map(lambda x: RuleGroup.get_rule_object(x).evaluate(obj), self.rules))
-        if self.condition == 'OR':
-            return any(map(lambda x: RuleGroup.get_rule_object(x).evaluate(obj), self.rules))
-        raise ValueError(f"Invalid condition: {self.condition}")
-
-    @staticmethod
-    def get_rule_object(rule):
-        """
-        Returns a RuleGroup object
-        if the 'rule' dictionary contains a 'rules' key with non-empty value,
-        otherwise returns a Rule object.
-
-        Args:
-            rule (dict): The rule dictionary.
-
-        Returns:
-            RuleGroup or Rule: The corresponding rule object.
-        """
-        if 'rules' in rule and len(rule['rules']) > 0:
-            return RuleGroup(rule)
-        return Rule(rule)
--- tests/rules/test_evaluator.py
+++ tests/rules/test_evaluator.py
@@ -1,4 +1,6 @@
 # pylint: disable=wrong-import-order
+from cluedin.rules.operators import get_operator
+
 from ..ctx import cluedin
 
 
@@ -25,3 +27,33 @@ class TestEvaluator:
         # Assert
 
         assert result is True
+
+    def test_custom_operators(self):
+
+        def custom_get_operator(operator_id):
+            if operator_id == 'compare_properties':
+                return lambda l, r, o: l == o.get(r) != None
+            return get_operator(operator_id)
+
+        # Arrange
+        rule = cluedin.json.load(
+            'tests/fixtures/rules/compare-properties.json')
+        entity0 = {
+            'name': 'The Godfather',
+            'entityType': '/IMDb/Title',
+            'imdb.title.primaryTitle': 'The Godfather',
+        }
+        entity1 = {
+            'name': 'The Godfather',
+            'entityType': '/IMDb/Title',
+            'imdb.title.primaryTitle': 'The Godfather II',
+        }
+
+        evaluator = cluedin.rules.Evaluator(
+            rule['data']['management']['rule']['condition'], custom_get_operator)
+
+        # Act
+        # Assert
+
+        assert evaluator.object_matches_rules(entity0) is True
+        assert evaluator.object_matches_rules(entity1) is False
